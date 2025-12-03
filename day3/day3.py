70d03dddfc237a248d409a8421cda5ffe32d99da
cowzix <github@onmyputer.com>
2025-12-03 16:20:10 -0500

    day 3 recursive

--- day3/day3.py
+++ day3/day3.py
@@ -1,27 +1,35 @@
 # Day 3
 def main():
-    with open('test.txt', 'r') as file:
+    with open('input.txt', 'r') as file:
         banks = file.readlines()
 
     total = 0
     total2 = 0
 
     for bank in banks:
-        bank = list(bank.strip())
-        total += jolt(bank, 2)
-        total2 += jolt(bank, 12)
+        bank = bank.strip()
+        total += int(jolt(bank, 2, ""))
+        total2 += int(jolt(bank, 12, ""))
 
     print(total, total2)
 
-def jolt(bank: list, size: int)-> int:
-    end = len(bank)
-    maxi = bank.index(max(bank[:end-size+1]))
-    jolt = f"{bank[maxi]}"
-    for i in range(1,size):
-        maxi2 = bank.index(max(bank[maxi+1:end-i+1]))
-        jolt += bank[maxi2]
+def jolt(bank: str, size: int, curr = ""):
+    if size > len(bank):
+        return None
+    if size == 0:
+        return curr
+    if size == 1:
+        return curr + max(bank)
+    
+    for d in "9876543210":
+        if d in bank:
+            i = bank.index(d)
+            search = bank[i+1:]
+            output = jolt(search, size -1, curr + d)
+            if output is not None:
+                return output
 
-    return int(jolt)
+    
 
 if __name__ == "__main__":
     main()
